09ed073559f241848b105f105b005c9615979af9
Manoj Pandey <manojpandey1996@gmail.com>
2016-05-25 20:49:59 +0530

    add masonry, add to notes

--- src/components/notes/Index.vue
+++ src/components/notes/Index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="notes">
+  <div class="notes" v-el:notes>
       <note
         v-for="note in notes"
         :note="note"
@@ -7,30 +7,42 @@
       </note>
   </div>
 </template>
+
 <script>
-// add js here
-import Firebase from 'firebase'
-import Note from './Note'
-export default {
-  components: {
-    Note
-  },
-  data () {
-    return {
-      notes: []
+  import Firebase from 'firebase'
+  import Masonry from 'masonry-layout'
+  import Note from './Note'
+  export default {
+    components: {
+      Note
+    },
+    data () {
+      return {
+        notes: []
+      }
+    },
+    ready () {
+      let masonry = new Masonry(this.$els.notes, {
+        itemSelector: '.note',
+        columnWidth: 240,
+        gutter: 16,
+        fitWidth: true
+      })
+      let firebase = new Firebase('https://keepg.firebaseio.com/')
+      firebase.child('notes').on('child_added', (snapshot) => {
+        let note = snapshot.val()
+        this.notes.unshift(note)
+        this.$nextTick(() => { // the new note hasn't been rendered yet, but in the nextTick, it will be rendered
+          masonry.reloadItems()
+          masonry.layout()
+        })
+      })
     }
-  },
-  ready () {
-    let firebase = new Firebase('https://keepg.firebaseio.com/')
-    firebase.child('notes').on('child_added', (snapshot) => {
-      let note = snapshot.val()
-      this.notes.unshift(note)
-    })
   }
-}
 </script>
+
 <style>
   .notes{
-    padding: 0 100px;
+    margin: 0 auto;
   }
 </style>
--- src/components/notes/Note.vue
+++ src/components/notes/Note.vue
@@ -19,9 +19,8 @@
     border-radius: 2px;
     box-shadow: 0 2px 5px #ccc;
     padding: 10px;
+    margin: 8px 0;
     width: 240px;
-    margin: 16px;
-    float: left;
   }
   .note h1{
     font-size: 1.1em;
